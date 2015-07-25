d063f79bfc8c819bf1babc9c67041363211c9852
Eric Liao <rcliao01@gmail.com>
2015-07-25 10:30:43 -0700

    update vote count

--- impulse/www/js/controllers.js
+++ impulse/www/js/controllers.js
@@ -2,7 +2,12 @@ angular.module('impulse.controllers', [])
 
 .controller('TheController', function() {
   var self = this;
-  var socket = io.connect('http://192.168.0.102:1337');
+  // configure the server ip
+  var SERVER_IP = 'localhost';
+  var socket = io.connect('http://' + SERVER_IP + ':1337');
+
+  var volume_vote_dom = document.querySelector('.volume .feedback-bar');
+  var genre_vote_dom = document.querySelector('.genre .feedback-bar');
 
   self.genres = ['Electronic', 'Jazz', 'Metal'];
   self.currentGenre = self.genres[0];
@@ -12,17 +17,21 @@ angular.module('impulse.controllers', [])
     self.currentGenres = currentState.genre;
     console.log('Got state: ');
     console.log(currentState);
+
+    updateVotes();
   });
 
   socket.on('volume', function(currentVolumeVote) {
     self.currentVolumeVote = currentVolumeVote;
     console.log('Got Volume ' + currentVolumeVote);
+    updateVotes();
   });
 
   socket.on('genre', function(genres) {
     self.currentGenres = genres;
     console.log('Got Genres');
     console.log(genres);
+    updateVotes();
   });
 
   self.upvote = function() {
@@ -65,4 +74,30 @@ angular.module('impulse.controllers', [])
     document.querySelector('.dropdown')
       .classList.toggle('open');
   };
+
+  function updateVotes () {
+    if (self.currentVolumeVote >= -10 && self.currentVolumeVote <= 10) {
+      var vote_dom_progress = volume_vote_dom.querySelector('.progress');
+      vote_dom_progress
+        .setAttribute('width', Math.abs(self.currentVolumeVote) * 15);
+      if (self.currentVolumeVote < 0) {
+        vote_dom_progress
+          .setAttribute('x', 156 - Math.abs(self.currentVolumeVote * 15));
+        vote_dom_progress
+          .setAttribute('style', 'fill: #FF3E3E');
+      } else {
+        vote_dom_progress
+          .setAttribute('style', 'fill: #00FF86');
+        vote_dom_progress
+          .setAttribute('x', 164);
+      }
+    }
+
+    genre_vote_dom.querySelector('.progress')
+      .setAttribute('width',
+        (self.currentGenres[self.currentGenre]) ?
+          self.currentGenres[self.currentGenre] * 15 :
+          0
+      );
+  }
 });
